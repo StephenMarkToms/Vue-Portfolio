7561e0a0040de7102a9c39587caa265a24a1dcda
Stephen Toms <stoms@DIGI5.local>
2018-11-13 21:40:15 -0500

    created smooth page transitions

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,23 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
-    <router-view/>
+    
+      <transition 
+        v-on:enter="enter" 
+        v-on:leave="leave"
+        v-bind:css="false"
+        appear
+      >
+        <template id="page">
+          
+          <router-view/>
+      
+        </template>
+      </transition>
+  
   </div>
 </template>
+
 
 <style>
 #app {
@@ -28,4 +42,48 @@
 #nav a.router-link-exact-active {
   color: #42b983;
 }
+
+.page {
+  position: absolute;
+  left: 0;
+  top: 100px;
+  width: 100vw;
+  height: 100vh;
+  background: #c0c0c0;
+}
+
 </style>
+
+
+
+<script>
+
+export default {
+  name: "app",
+  template: '#page',
+	methods: {
+		enter: function enter(el, done) {
+			TweenMax.fromTo(el, 1, {
+				autoAlpha: 0,
+				scale: 1.5 },
+			{
+				autoAlpha: 1,
+				scale: 1,
+				transformOrigin: '50% 50%',
+				ease: Power4.easeOut,
+				onComplete: done });
+
+		},
+		leave: function leave(el, done) {
+			TweenMax.fromTo(el, 1, {
+				autoAlpha: 1,
+				scale: 1 },
+			{
+				autoAlpha: 0,
+				scale: 0.8,
+				ease: Power4.easeOut,
+				onComplete: done });
+
+		} }
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,26 @@
+/* eslint-disable */
+
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+
+// import 'bootstrap/dist/js/bootstrap'
+import 'bootstrap';
+
+// import plugins individually - require exports-loader
+import 'bootstrap/js/dist/modal'
+import 'bootstrap/js/dist/tooltip'
+
+// import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/scss/bootstrap.scss'
+
+//import GreenSock
+import 'gsap/TweenMax'
+
+// jquery
+global.$ = $;
+
 
 Vue.config.productionTip = false;
 
@@ -10,3 +29,5 @@ new Vue({
   store,
   render: h => h(App)
 }).$mount("#app");
+
+
