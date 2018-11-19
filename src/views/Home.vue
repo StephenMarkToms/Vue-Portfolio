ab8fa497496e4aab7af74f5ac3affa7fb742165e
Stephen Toms <stoms@DIGI5.local>
2018-11-18 20:40:50 -0500

    finished homepage animation

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -15,36 +15,45 @@
             
 
             <div class="callout">
-                <h1 id="line-1" class="text-white text-left" style="color: rgb(86, 86, 86) !important;">
-                    The world doesn't just need another photographer videographer developer <span class="text-white">The world doesn't just need another</span> photographer videographer developer
+                <h1 id="line-1" class="text-left" style="color: rgb(86, 86, 86) !important;">
+                   <span class="remove-text">
+                     The world doesn't just need another photographer videographer developer The world doesn't just need another photographer videographer developer The world doesn't just need another photographer videographer developer 
+                   </span>
+                     <span class="keep-text">The world doesn't just need another</span>
+                     <span class="remove-text">
+                        photographer videographer developer
+                     </span>
+
                 </h1>
 
                 <h1 id="line-2" class="text-white text-left" style="color: rgb(86, 86, 86) !important;">
-                    The world doesn't just need another photographer videographer developer The world doesn't just need another <span class="text-white">photographer videographer developer</span> The world doesn't just need another ph
+                   <span class="remove-text">
+                     e world doesn't just need another 
+                   </span>
+                    <span class="keep-text">photographer videographer developer</span>
+                    <span class="remove-text">
+                     The world doesn't just need another photographer videographer developer The world doesn't just need another photographer videographer developer The world doesn't just need another photographer videographer developer 
+                    </span>
                 </h1>
 
                 <h1 id="line-3" class="text-white text-left" style="color: rgb(86, 86, 86) !important;">
-                    The world doesn't just need another photographer videographer developer The world doesn't just need another <span class="text-white">It needs a visionary</span> The world doesn't just need another photographer videographer developer
+                    <span class="text-white">It needs a visionary</span>
                 </h1>             
             </div>
 
-            <div class="container">
+            <div id="home-content" class="container">
               
-              <div class="row mt-5 callout-row">
-                <div class="col-12 col-md-8 ml-2 mt-5">
+              <div class="row mt-5">
+                <div class="col-12 ml-2 mt-5">
                   
-
 
                 </div>
               </div>
+
             </div>
 
            
-            <div class="row mt-5">
-              <div class="col">
-                <button>Start</button>
-              </div>
-            </div>
+            
 
 
 
@@ -96,44 +105,99 @@ export default {
         onComplete: done });
         
 
-
+      animateCallout();
 
 
        
        
           
-        $('button').click( function(){
+        function animateCallout(){
         
           //initial set
           TweenMax.set(
               $('#line-1'),
                 {
-                  x: "-50%",
-                  scaleX: 1,
-                  scaleY: 1,
+                  x: "-5%",
+                  scaleX: 2,
+                  scaleY: 2,
+                  margin: "50 0 0 50",
                 }
               );
 
           TweenMax.set(
               $('#line-2'),
                 {
-                  x: "-58.8%",
-                  scaleX: 1,
-                  scaleY: 1,
-                }
-              );
-
-          TweenMax.set(
-              $('#line-3'),
-                {
-                  x: "-54%",
-                  scaleX: 1,
-                  scaleY: 1,
+                  x: "-70%",
+                  scaleX: 2,
+                  scaleY: 2,
+                  margin: "50 0 0 50",
                 }
               );
     
+          //end
+          TweenMax.to(
+              $('#line-1'),
+              2.5,
+                {
+                  x: "-75%",
+                  scaleX: 1,
+                  scaleY: 1,
+                  ease: Power2.easeOut,
+                  margin: "0 0 0 50",
+                }
+              );
+
+          TweenMax.to(
+              $('#line-2'),
+              2.5,
+                {
+                  x: "-11.2%",
+                  scaleX: 1,
+                  scaleY: 1,
+                  ease: Power2.easeOut,
+                  margin: "0 0 0 50",
+                }
+              );
+
+          TweenMax.to(
+              $('.remove-text'),
+              .75,
+                {
+                  delay: 2,
+                  alpha: 0,
+                  ease: Power2.easeOut,
+                }
+              );
+
+          TweenMax.to(
+              $('.keep-text'), .5,
+                {
+                  delay: 1.5,
+                  css:{color:"white"},
+                  ease: Power4.easeInOut
+                }
+              );
           
-        });
+          TweenMax.to(
+              $('.keep-text'), 3,
+                {
+                  delay: 3,
+                  css:{color:"rgb(86, 86, 86)"},
+                  ease: Power4.easeInOut
+                }
+              );
+
+          var tl = new TimelineLite, 
+          mySplitText = new SplitText("#line-3", {type:"words,chars"}), 
+          chars = mySplitText.chars; //an array of all the divs that wrap each character
+
+          TweenLite.set("#line-3", {perspective:400});
+
+          tl.staggerFrom(chars, 1.5, {delay: 3, opacity:0, scale:0, y:80, rotationX:180, transformOrigin:"0% 50% -50",  ease:Back.easeOut}, 0.01, "+=0");
+
+
+          
+        }
 
 
         //end custom
