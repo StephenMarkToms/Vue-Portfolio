42b0e6966ab01cf9942bfa5c79140acf4d94d2d3
Stephen Toms <stoms@DIGI5.local>
2018-11-22 13:58:52 -0500

    added missing assets

---
+++ src/components/CaseStudy.vue
@@ -0,0 +1,40 @@
+<template>
+
+      
+
+      <div class="case-study" v-bind:style="{ backgroundImage: 'url(' + this.bg + ')' }">
+
+        <h1>{{ name }}</h1>
+        <h1>{{ name }}</h1>
+        <h1>{{ name }}</h1>
+        <h1>{{ name }}</h1>
+        <h1>{{ name }}</h1>
+        <div class="img-banner" :style="{'background-image': 'url(' + require('../assets/' + icon + '.jpg') + ')'}">
+
+          adsfasdfsad
+        
+        </div>
+
+      </div>
+
+</template>
+
+<script>
+export default {
+  name: "CaseStudy",
+  bgSrc: "./assets/imgs/case-study-1.jpg",
+  props: {
+    name: String,
+    bg: String,
+    icon: String
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+
+
+
+</style>
